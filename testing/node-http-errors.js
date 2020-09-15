3da0ad7055fe9ec1bc712086e59d7292acc71443
Dirk Viljoen <dirkcharl.viljoen@gmail.com>
2020-09-15 07:08:50 +0200

    Create node-http-errors.js
    The server allows testing common http errors

---
+++ testing/node-http-errors.js
@@ -0,0 +1,30 @@
+/*
+  Start the server with `node node-http-errors.js`
+  A list of endpoints will be displayed that results in the relevant http errors
+*/
+const paths = ['/ECONRESET'];
+
+const pathMap = (port) => paths.map(m => `http://localhost:${port}${m}`).join('\n');
+
+const server = require('http')
+  .createServer((req, res) => {
+
+    switch (req.url) {
+      case '/ECONRESET':
+        console.log('Incomming request... Closing socket.');
+        req.socket.destroy();
+        break;
+    }
+
+    res.write(`try the following: \n ${pathMap(serverAddress.port)}`); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(1338);
+const serverAddress = server.address();
+
+console.log(`running server on http://localhost:${serverAddress.port}`);
+console.log(`Try one of the following paths:
+
+${pathMap(serverAddress.port)}
+
+`);
